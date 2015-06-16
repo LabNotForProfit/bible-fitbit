6e976c98cc18d865342a7de7ba74f383453f64dc
david <davidwosk1@gmail.com>
2015-06-16 10:05:56 -0700

    include Devise::Controllers in application controller

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module BibleFitbit
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.action_controller.include_all_helpers = false
+    # config.action_controller.include_all_helpers = false
   end
 end
