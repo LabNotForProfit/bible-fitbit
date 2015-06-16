e176c37eed86728a10b329fc3dcbca6b45348179
david <davidwosk1@gmail.com>
2015-06-16 10:10:42 -0700

    include helpers function in application controller for heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  include Devise::Controllers
+  # include Devise::Controllers
+  Devise.include_helpers(Devise::Controllers)
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
