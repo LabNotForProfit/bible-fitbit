1731135abef44a37d7c772fd1fb87beb7065614e
david <davidwosk1@gmail.com>
2015-06-16 14:56:30 -0700

    duplicate devise_for :users. take out path '/'. leave api in uri

--- config/routes.rb
+++ config/routes.rb
@@ -64,7 +64,7 @@ Rails.application.routes.draw do
   #   end
   devise_for :users
   # namespace :api, path: '/' do
-  namespace :api, path: '/' do
+  namespace :api do
     devise_for :users
   end
 end
