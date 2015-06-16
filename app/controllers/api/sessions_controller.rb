c9101f0705cd0bd5ccc55c1e40aeff31e55709a2
david <davidwosk1@gmail.com>
2015-06-15 21:05:33 -0700

    api for destroying session

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,5 +1,6 @@
 class Api::SessionsController < Devise::SessionsController
-prepend_before_filter :require_no_authentication, :only => [:create ]
+prepend_before_filter :require_no_authentication, :only => [:create]
+protect_from_forgery
 #include Devise::Controllers
 before_filter :ensure_params_exist, :only => :create
  
@@ -15,6 +16,7 @@ end
 # POST /users/sign_in
 def create
 #build_resource
+
 resource = User.find_for_database_authentication(:email=>params[:api_user][:email])
 return invalid_login_attempt unless resource
  
@@ -24,15 +26,20 @@ render :json=> {:success=>true, :email=>resource.email}
 return
 end
 invalid_login_attempt
+
 end
 
-# def destroy
-# #sign_out(resource_name)
-# signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-# set_flash_message :notice, :signed_out if signed_out && is_flashing_format?
-# yield if block_given?
-# respond_to_on_destroy
-# end
+def destroy
+#sign_out(resource_name)
+signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+if signed_out
+set_flash_message :notice, :signed_out if signed_out && is_flashing_format?
+yield if block_given?
+respond_to_on_destroy
+else
+	render :json=>{:success=>false, :message=>"logout not successful"}
+end
+end
  
 protected
 def ensure_params_exist
@@ -45,4 +52,22 @@ def invalid_login_attempt
 warden.custom_failure!
 render :json=> {:success=>false, :message=>"Error with your login or password"}, :status=>401
 end
+
+private
+
+  def respond_to_on_destroy
+# We actually need to hardcode this as Rails default responder doesn't
+# support returning empty response on GET request
+respond_to do |format|
+# format.all { head :no_content }
+# format.any(*navigational_formats) { redirect_to after_sign_out_path_for(resource_name) }
+format.json{
+	render :json => {:success=>true, :message=>"signed out"}
+}
+format.html{
+	redirect_to after_sign_out_path_for(resource_name)
+}
+end
+end
+
 end 
