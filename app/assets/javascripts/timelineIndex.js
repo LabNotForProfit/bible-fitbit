8e6f5879d9d9ea25f9412fc0ee13c2a3f7fc9ddd
Martin Len <mlen1990@Martins-MacBook-Air-2.local>
2015-06-22 15:15:57 -0700

    Fixed scrolling for books with spaces

--- app/assets/javascripts/timelineIndex.js
+++ app/assets/javascripts/timelineIndex.js
@@ -4,7 +4,7 @@ $(function () {
 	$('.cd-date').each(function() {
 		var book = $(this).text();
 		if ($.inArray(book, books) === -1) {
-			books.push($(this).text())
+			books.push($(this).text());
 		}
 	});
 	var $bookSearch = $('#bookSearch');
@@ -12,7 +12,10 @@ $(function () {
 		source: books
 	})
 	$bookSearch.bind('enterKey', function(e) {
-		$(document).scrollTop($('#' + $('#bookSearch').val()).offset().top);
+		var url = window.location.href;
+		var urlInfo = url.split('#');
+		console.log(urlInfo[0] + '#' + $('#bookSearch').val().split(' ').join(''));
+		window.location.href = urlInfo[0] + '#' + $('#bookSearch').val().split(' ').join('');
 	});
 	$bookSearch.keyup(function(e) {
 		if (e.keyCode == 13) {
