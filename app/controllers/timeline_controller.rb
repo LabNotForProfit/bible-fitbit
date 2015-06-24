74fe44c6ffc95fb1ecd9f1df84ec824b00902aae
david <davidwosk1@gmail.com>
2015-06-24 14:03:43 -0700

    change query to Book in timeline controller to sort by id for timeline edit page to be ordered correctly

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -9,15 +9,12 @@ class TimelineController < ApplicationController
 
   def edit
   	@user = current_user
+    @books = Book.order(:id)
   end
 
   def update
     @biblesearch = BibleSearch.new('PPBuBK5LCmR4KLIsbytjtvCPDWbLkoSagxJhzQ6u')
     @book = Book.find_by_name(params[:book])
-    puts @book.name
-    puts @book.abbr
-    puts @book.key_verse
-    puts @verse
     # @passages = @biblesearch.passages('John 15:5', :version => "eng-ESV")
     @passages = @biblesearch.passages((@book.abbr+' '+@book.key_verse), :version => "eng-ESV")
 
