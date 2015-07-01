d215b079322ad409841200c689c6a50da03c3bfc
Martin Len <mlen1990@Martins-MacBook-Air-3.local>
2015-06-30 21:20:32 -0700

    Issue 65 - Fixed timeline page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,12 +3,12 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( leaderboard/leaderboardsIndex.js )
+Rails.application.config.assets.precompile += %w( timeline/index/timelineIndex.js )
 # Rails.application.config.assets.precompile += %w( friends/friends.js )
 # Rails.application.config.assets.precompile += %w( timeline/timeline.js )
 
 Rails.application.config.assets.precompile += %w( dashboard/dashboard.css )
 # Rails.application.config.assets.precompile += %w( leaderboard/leaderboard.css )
-Rails.application.config.assets.precompile += %w( timeline/timeline.js )
 Rails.application.config.assets.precompile += %w( timeline/timeline.css )
 # Rails.application.config.assets.precompile += %w( homeIndex.js )
 # Rails.application.config.assets.precompile += %w( jquery.dataTables.js )
