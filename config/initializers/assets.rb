d7fda17ccf98a97844cc951fcc3c2dfd9540823b
Martin Len <mlen1990@Martins-MacBook-Air-3.local>
2015-06-30 21:30:31 -0700

    Issue 65 - Fixed add progress page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,19 +4,16 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( leaderboard/leaderboardsIndex.js )
 Rails.application.config.assets.precompile += %w( timeline/index/timelineIndex.js )
+Rails.application.config.assets.precompile += %w( timeline/edit/timelineEdit.js )
 # Rails.application.config.assets.precompile += %w( friends/friends.js )
-# Rails.application.config.assets.precompile += %w( timeline/timeline.js )
 
 Rails.application.config.assets.precompile += %w( dashboard/dashboard.css )
 # Rails.application.config.assets.precompile += %w( leaderboard/leaderboard.css )
 Rails.application.config.assets.precompile += %w( timeline/timeline.css )
 # Rails.application.config.assets.precompile += %w( homeIndex.js )
-# Rails.application.config.assets.precompile += %w( jquery.dataTables.js )
 # Rails.application.config.assets.precompile += %w( allBadges.js )
-# Rails.application.config.assets.precompile += %w( timeline/timeline.js )
 # Rails.application.config.assets.precompile += %w( jquery.classywiggle.js )
 # Rails.application.config.assets.precompile += %w( jquery.knob.js )
-# Rails.application.config.assets.precompile += %w( leaderboardsIndex.js )
 # Rails.application.config.assets.precompile += %w( friendshipsIndex.js )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
