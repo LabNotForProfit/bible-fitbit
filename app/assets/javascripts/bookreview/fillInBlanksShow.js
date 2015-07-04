5124f2fd87fb6a22ef066fc471aaff2ce7ad9a49
Martin Len <mlen1990@Martins-MacBook-Air-3.local>
2015-07-04 15:24:42 -0700

    Randomized test

--- app/assets/javascripts/bookreview/fillInBlanksShow.js
+++ app/assets/javascripts/bookreview/fillInBlanksShow.js
@@ -1,8 +1,7 @@
-var passages = [];
 var verses = {};
 
 function checkAnswers() {
-	$('.blanks-icon').removeClass().addClass('blanks-icon');
+	$('.blanks-hidden').removeClass();
 	$('.blanks-chapter-answer').each(function(index, answer) {
 		var $answer = $(answer);
 		var $icon = $answer.next();
@@ -13,12 +12,33 @@ function checkAnswers() {
 			$icon.addClass('red fa fa-close');
 		}
 	});
+	$('#checkAnswers').text('Review Again').attr('onclick', 'reset()');
+}
+
+function reset() {
+	window.location.href = window.location.href;
+}
+
+function shuffle(array) {
+	var currentIndex = array.length, temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+	return array;
 }
 
 $(function() {
-	console.log('fillInBlanks.js');
 	$('.v, .s1, .b').remove();
-	passages = $('.show-passages').remove();
+	var passages = shuffle($('.show-passages').remove());
 	var byChapter = '<h3>Pick the correct chapter</h3>';
 	$.each(passages, function(index, passage) {
 		var children = $(passage).children();
@@ -30,12 +50,13 @@ $(function() {
 		verses[reference] = verse.join(' ').replace(/”/g, '" ').replace(/“/g, '"').replace(/‘/g, "'").replace(/’/g, "'").replace(/ +/g, ' ').trim();
 		byChapter += '<div class="row blanks-padding">';
 		byChapter += '<div class="col-xs-1"><input id="' + reference + '" class="blanks-chapter-answer"><div class="blanks-icon"></div></div>	';
-		byChapter += '<div class="col-lg-10">' + verses[reference]+ '</div>';
+		byChapter += '<div class="col-lg-9">' + verses[reference] + '</div>';
+		byChapter += '<div class="col-xs-2 blanks-hidden">' + reference + '</div>';
 		byChapter += '</div>';
 	});
 	byChapter += '<div class="row blanks-padding">';
 	byChapter += '<div class="col-xs-1"></div>';
-	byChapter += '<div class="col-lg-10 text-right">' + '<button class="btn btn-default btn-lg ghost-button-twitter" onclick="checkAnswers();">Check Answers</button>' + '</div>';
+	byChapter += '<div class="col-lg-10 text-right">' + '<button id="checkAnswers" class="btn btn-default btn-lg ghost-button-twitter" onclick="checkAnswers();">Check Answers</button>' + '</div>';
 	byChapter += '</div>';
 	$('#pickChapter').html(byChapter);
 });
