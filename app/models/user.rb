a9c624221789c43ce94f7bfe1a1ccfb9c775149a
Nathan Connor <natyconnor@gmail.com>
2015-07-08 14:51:04 -0700

    Set up Paperclip gem, config, model, and db. May need something different for development vs production

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,18 @@ class User < ActiveRecord::Base
   EMAIL_REGEX = /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   validates :username, :presence => true, :uniqueness => true, :length => {:in => 1..20}, :format => UNAME_REGEX
 
+  # Paperclip
+  # This method associates the attribute ":avatar" with a file attachment
+  has_attached_file :avatar, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
+
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
