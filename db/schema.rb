2001e665fbcbcee345c97092e99cd037f70e9ff1
Nathan Connor <natyconnor@gmail.com>
2015-07-31 14:29:54 -0700

    Changed quiz score to have num_questions and num_correct instead of just score

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150725185158) do
+ActiveRecord::Schema.define(version: 20150731192007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,9 +58,10 @@ ActiveRecord::Schema.define(version: 20150725185158) do
   create_table "quiz_scores", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "book_id"
-    t.float    "score"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "num_questions"
+    t.integer  "num_correct"
   end
 
   create_table "studied_books", force: :cascade do |t|
