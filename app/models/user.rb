59218e172402733c054b866297959f712101fac3
david <davidwosk1@gmail.com>
2015-08-22 23:39:35 -0700

    take out username field from sign up. back end always set username to be local part of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
 
   UNAME_REGEX = /\A(\w|\.)+\z/
   EMAIL_REGEX = /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  validates :username, :presence => true, :uniqueness => true, :length => {:in => 1..20}, :format => UNAME_REGEX
+  # validates :username, :presence => true, :uniqueness => true, :length => {:in => 1..20}, :format => UNAME_REGEX
 
   # Paperclip
   # This method associates the attribute ":avatar" with a file attachment
