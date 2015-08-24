7ce49d510559ecfcdf82b39c8ce9d67bf3fa2aae
david <davidwosk1@gmail.com>
2015-08-23 23:59:34 -0700

    admin search bar autocomplete

--- app/controllers/api/admin_controller.rb
+++ app/controllers/api/admin_controller.rb
@@ -2,6 +2,15 @@ class Api::AdminController < ApplicationController
 
   def manage_users
   	@users = User.all
+
+  	respond_to do |format|
+      format.json {
+        render :json => @users.as_json(:only => [:id, :username, :firstname, :lastname, :email], :methods => [:avatar_url])
+      }
+      format.html {
+        render 'manage_users'
+      }
+    end
   end
 
   def change_settings
